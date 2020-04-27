52778e2099cd5ddc5c25a21b20bd6617d704b80c
CodingWith-Adam <adam@example.com>
2020-04-26 20:44:57 -0600

    Updates

---
+++ precache-manifest.e427e5ded1b72ffaffcc7a1db01f08b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c20fb32fa2707c9b02b1ef8df1f3a24",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f32f99e19302b621614a",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f0f6b96b5c9935c892a6",
+    "url": "/my-app/static/js/2.b13ae2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.b13ae2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32f99e19302b621614a",
+    "url": "/my-app/static/js/main.b604c220.chunk.js"
+  },
+  {
+    "revision": "6b90711d51ae396ac389",
+    "url": "/my-app/static/js/runtime-main.8c451e69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
